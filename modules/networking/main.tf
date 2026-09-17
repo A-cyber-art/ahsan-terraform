876cf8668007b25b5aa963958ef6b7146827fde8
Ahsan Imran <146340222+A-cyber-art@users.noreply.github.com>
2026-09-17 11:00:46 +0500

    Create main.tf

---
+++ modules/networking/main.tf
@@ -0,0 +1,29 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_security_group" "ecs_sg" {
+  name   = var.security_group_name
+  vpc_id = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = var.container_port
+    to_port     = var.container_port
+    protocol    = "tcp"
+    cidr_blocks = var.allowed_cidr_blocks
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = var.allowed_cidr_blocks
+  }
+}
